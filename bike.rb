3e76faf3d69ec5ceb9372c7e342c6fa7a83e502e
Toytum <steath1999@gmail.com>
2020-03-18 11:18:50 -0700

    adds bike parent class references #1

---
+++ bike.rb
@@ -0,0 +1,19 @@
+
+
+class Bike
+
+def initialize(equipment = nil, hourly_rate = 5, daily_rate = 15, weekly_rate = 70)
+    @equipment = equipment
+    @hourly_rate = hourly_rate
+    @daily_rate = daily_rate
+    @weekly_rate = weekly_rate
+end
+
+def clean
+    puts "Cleaning..."
+end
+
+
+
+
+end
